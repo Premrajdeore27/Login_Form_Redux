8b9ef0857ccde39f66583bbc8d0b1c6bc0c69ca7
Premraj Deore <86760579+Premrajdeore27@users.noreply.github.com>
2022-06-26 18:21:54 +0530

    change

--- src/App.js
+++ src/App.js
@@ -14,34 +14,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import Login from './Components/Login'
-// import Logout from './Components/Logout'
-
-// const App=()=>{
-//   return(
-//     <div>
-//       <Login/>,
-//       <Logout/>
-//     </div>
-//   )
-// }
-
-// export default App
